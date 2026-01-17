6ff98554d287bb2409241ae3d69dea5e4e423240
Stalin Amagua <jueltter@users.noreply.github.com>
2026-01-17 04:19:26 -0500

    - capacitor added - android project added

---
+++ android/app/src/main/java/tech/samagua/ekumen_tourism_tracker_prototype_client/MainActivity.java
@@ -0,0 +1,5 @@
+package tech.samagua.ekumen_tourism_tracker_prototype_client;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tech.samagua.ekumen_tourism_tracker_prototype_client',
+  appName: 'TourismTracker',
+  webDir: 'dist/ekumen-tourism-tracker-prototype-client/browser',
+};
+
+export default config;

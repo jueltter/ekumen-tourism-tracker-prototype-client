31436f8b8c45c0eaaa4ced7e5d4afdc77bb70b70
Stalin Amagua <stalin.amagua@outlook.com>
2026-06-10 01:11:20 -0500

    achievement service refactored

--- src/app/shared/tourism-tracker/services/achievement.service.ts
+++ src/app/shared/tourism-tracker/services/achievement.service.ts
@@ -1,14 +1,18 @@
 import { inject, Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import {map, Observable, tap} from 'rxjs';
 import { EnvironmentService } from 'src/app/shared/services/environment.service';
 import { Achievement } from '@tourism-tracker-interfaces/achievement.interface';
+import { RequestResponse } from '@tourism-tracker-interfaces/request-response.interface';
+import { LoggerWrapperService } from '@common-services/logger-wrapper.service';
 
+type AchievementArrayResponse = RequestResponse<Achievement[]>;
 
 @Injectable({
   providedIn: 'root',
 })
 export class AchievementService {
+  private logger = inject(LoggerWrapperService);
   private environmentService = inject(EnvironmentService);
   private url = this.environmentService.backendUrl + '/api/achievements';
 
@@ -17,7 +21,11 @@ export class AchievementService {
   search(): Observable<Achievement[]> {
     let params = new HttpParams();
 
-    return this.http.get<Achievement[]>(this.url, {params});
+    return this.http.get<AchievementArrayResponse>(this.url, {params}).pipe(
+      tap(x => this.logger.info('response:', x)),
+      map(x => x.data),
+      tap(x => this.logger.info('Fetched achievements:', x.length))
+    );
   }
 
 }
